95d811c239be325e49afd2f1dfef69062efcf802
S1im_Sh4dy <mihakurd@gmail.com>
2022-09-22 15:01:37 +0300

    Мелкие исправления

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0fnb(55d64@oe#e2753uq7k8jo)-3#=gis!4c-chbizci*hf#j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
