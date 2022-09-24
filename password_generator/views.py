b17a08adc5c68dd614e10ab11706954ce13eedab
S1im_Sh4dy <mihakurd@gmail.com>
2022-09-24 22:28:32 +0300

    Fix generate password

--- password_generator/views.py
+++ password_generator/views.py
@@ -8,21 +8,27 @@ def home(request):
 
 
 def password(request):
-    Chars = list('abcdefghijklmnopqrstuvwxyz')
+    Chars = {
+        'simple': list('abcdefghijklmnopqrstuvwxyz'),
+        'upper': list('abcdefghijklmnopqrstuvwxyz'.upper()),
+        'numbers': list('0123456789'),
+        'special': list('~!@#$%^&*()+\'`;:<>/\|'),
+    }
     length_password = int(request.GET.get('length', 8))
 
+    the_password = ''
     if request.GET.get('uppercase'):
-        Chars += ''.join(Chars).upper()
+        the_password += random.choice(Chars['upper'])
 
     if request.GET.get('numbers'):
-        Chars.extend(list('0123456789'))
+        the_password += random.choice(Chars['numbers'])
 
     if request.GET.get('special_char'):
-        Chars.extend(list('~!@#$%^&*()+\'`;:<>/\|'))
+        the_password += random.choice(Chars['special'])
 
-    the_password = ''
-    for char in range(length_password):
-        the_password += random.choice(Chars)
+    for char in range(len(the_password), length_password):
+        the_password += random.choice(Chars['simple'])
+
+    the_password = ''.join(random.sample(list(the_password), len(the_password)))
 
     return render(request, 'password_generator/password.html', {'password': the_password})
-
